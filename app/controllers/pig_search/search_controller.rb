a52a5fbf570b19a844160e9d11f85e4eabe06544
Daniel King <dan@yoomee.com>
2016-06-28 15:50:06 +0100

    add basic filtering by tag

--- app/controllers/pig_search/search_controller.rb
+++ app/controllers/pig_search/search_controller.rb
@@ -2,10 +2,22 @@ module PigSearch
   class SearchController < ::ApplicationController
     def search
       @results = Elasticsearch::Model.search(search_params[:query]).results
+      @tags = get_tags_with_counts(@results)
+      @results = filter_results_by_tag(@results, search_params[:filter]) if search_params[:filter].present?
     end
 
+    private
     def search_params
-      params.permit(:query)
+      params.permit(:query, :filter)
+    end
+
+    def get_tags_with_counts(results)
+      tags = results.flat_map(&:result_tags)
+      tags.uniq.collect{|tag| [tag, tags.count(tag)]}
+    end
+
+    def filter_results_by_tag(results, tag)
+      results.select{|result| result.result_tags.include?(tag)}
     end
   end
 end
